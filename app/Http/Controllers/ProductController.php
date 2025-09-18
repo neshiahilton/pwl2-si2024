afcee1ee61bbc7d050d709397f90d281687001e1
Neshia Hilton <taneshia.4220240022@civitas.ukrida.ac.id>
2025-09-18 14:52:35 +0700

    My First Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+    * index
+    *
+    * @return void
+    */
+    public function index() : View
+    {
+        //get all products
+        $product = new Product;
+        $products = $product->get_product()->latest()->paginate(10);
+
+        //render view with products
+        return view('products.index', compact('products'));
+    }
+}
